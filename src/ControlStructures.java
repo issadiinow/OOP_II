9459f3cca58c252faf48a6c63c5034e167f20055
Issa Mohamed <issa.mohamed@student.moringaschool.com>
2025-04-28 22:05:41 +0300

    completed challenge 3

--- src/ControlStructures.java
+++ src/ControlStructures.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class ControlStructures {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the If-Else Demo!");
+
+        System.out.print("Enter a day of the week: ");
+        String day = scanner.nextLine().toLowerCase();
+
+        if (day.equals("saturday") || day.equals("sunday")) {
+            System.out.println("It's the weekend!");
+        } else {
+            System.out.println("It's a weekday.");
+        }
+
+        scanner.close();
+    }
 }

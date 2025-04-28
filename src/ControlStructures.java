859a556b6f4767fe5bd43dd85c545c2ad1de6fd8
Issa Mohamed <issa.mohamed@student.moringaschool.com>
2025-04-28 11:22:32 +0300

    Initial Project SetUp

---
+++ src/ControlStructures.java
@@ -0,0 +1,2 @@
+public class ControlStructures {
+}
---
+++ src/WhileLoop.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class WhileLoop {
+    Scanner sc = new Scanner(System.in);
+
+    void showHello(){
+        String choice;
+        System.out.println("For hello type 'h', anything else to stop");
+        choice = sc.next();
+        while ("h".equals(choice)){
+            System.out.println("Hello");
+            choice = sc.next();
+        }
+    }
+    public static void main(String[] args) {
+        WhileLoop obj = new WhileLoop();
+        obj.showHello();
+    }
+}
